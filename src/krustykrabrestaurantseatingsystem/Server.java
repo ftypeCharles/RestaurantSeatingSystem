1c752bda5b05d71a94667ddd84976acf8fde79ae
charleslyding <charleslyding@charless-mbp.home>
2017-11-16 11:28:14 -0500

    Adding Table Class

---
+++ src/krustykrabrestaurantseatingsystem/Server.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krustykrabrestaurantseatingsystem;
+
+/**
+ *
+ * @author charleslyding
+ */
+public class Server extends Employee{
+    
+    private String assignedSection;
+    private int totalTips;
+    
+    public void chargeParty(){
+        
+    }
+    
+}
